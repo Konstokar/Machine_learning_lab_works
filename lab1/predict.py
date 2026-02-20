b1e514b14c883f0e51ea340689bbe64b689441b4
Konstokar <45202593+Konstokar@users.noreply.github.com>
2026-02-20 16:11:30 +0400

    4th commit: lab1 cosmetics changes

--- lab1/predict.py
+++ lab1/predict.py
@@ -7,20 +7,24 @@ IMG_SIZE = (64, 64)
 
 model = tf.keras.models.load_model("ffnn_model.keras")
 
-# загружаем реальные классы
 with open("class_names.json", "r") as f:
     class_names = json.load(f)
 
 
 def predict(img_path):
     img = image.load_img(img_path, target_size=IMG_SIZE)
-    img_array = image.img_to_array(img) / 255.0
+
+    img_array = image.img_to_array(img)
+    img_array = img_array / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
     prediction = model.predict(img_array)
+
     class_id = np.argmax(prediction)
+    confidence = np.max(prediction)
 
-    print("Prediction:", class_names[class_id])
+    print(f"Prediction: {class_names[class_id]}")
+    print(f"Confidence: {confidence:.2f}")
 
 
-predict("test.jpg")
+predict("horse_test.jpg")
